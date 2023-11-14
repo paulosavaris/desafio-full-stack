1a2098f6821acf01a27141a35ab90aa255d40de5
paulosavaris <pauloosavaris@gmail.com>
2023-11-14 13:58:05 -0300

    Criado entidade Usuario.java. Configurado para criar tabelas no banco de dados baseada na entidade. Funcionando por hora.

---
+++ src/main/java/br/edu/unoesc/desafiofullstack/Entidades/Usuario.java
@@ -0,0 +1,32 @@
+package br.edu.unoesc.desafiofullstack.Entidades;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+}
